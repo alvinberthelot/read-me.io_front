ea3d005bc2fcde1198c93db75d3d0ed28b5fe8c9
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-04-12 16:30:37 +0200

    Add Prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
-import { MatAutocomplete } from '@angular/material';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs/Observable';
-import { startWith } from 'rxjs/operators/startWith';
-import { map } from 'rxjs/operators/map';
-import { ReadMe } from './services/readMe.service';
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
-import { extend } from 'webdriver-js-extender';
-import { environment } from '../environments/environment';
+import { Component } from '@angular/core'
+import { FormControl } from '@angular/forms'
+import { Observable } from 'rxjs/Observable'
+import { startWith } from 'rxjs/operators/startWith'
+import { map } from 'rxjs/operators/map'
+import { ReadMe } from './services/readMe.service'
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks'
 
 @Component({
   selector: 'app-root',
@@ -15,52 +12,43 @@ import { environment } from '../environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  templateControl: FormControl = new FormControl()
+  filteredOptions: Observable<string[]>
+  templates = []
+  extensions = []
+  readmeFile: string
 
-  templateControl: FormControl = new FormControl();
-  filteredOptions: Observable<string[]>;
-  templates = [];
-  extensions = [];
-  readmeFile: string;
-
-  constructor(private readMe: ReadMe) {
-  }
+  constructor(private readMe: ReadMe) {}
 
   ngOnInit() {
-    this.getTemplates();
-    this.getExtention();
+    this.getTemplates()
+    this.getExtention()
   }
 
   submitButton(template: any, extension: any) {
-    console.log(template, extension);
-    this.readMe.getReadme(template, extension).subscribe( readme => {
-      this.readmeFile = readme;
-    });
+    console.log(template, extension)
+    this.readMe.getReadme(template, extension).subscribe(readme => {
+      this.readmeFile = readme
+    })
   }
-
 
   getTemplates() {
     this.readMe.getTemplates().subscribe(result => {
-      this.templates = result;
+      this.templates = result
 
-      this.filteredOptions = this.templateControl.valueChanges
-      .pipe(
-        startWith(''),
-        map(val => this.filterLowerCase(val))
-      );
-    });
+      this.filteredOptions = this.templateControl.valueChanges.pipe(startWith(''), map(val => this.filterLowerCase(val)))
+    })
   }
 
   getExtention() {
     this.readMe.getExtention().subscribe(result => {
-      result
-        .map(item => {
-          this.extensions.push({value: item, viewValue: '.' + item});
-        });
-    });
+      result.map(item => {
+        this.extensions.push({ value: item, viewValue: '.' + item })
+      })
+    })
   }
 
   filterLowerCase(val: string): string[] {
-    return this.templates.filter(option =>
-      option.toLowerCase().indexOf(val.toLowerCase()) === 0);
+    return this.templates.filter(option => option.toLowerCase().indexOf(val.toLowerCase()) === 0)
   }
 }
