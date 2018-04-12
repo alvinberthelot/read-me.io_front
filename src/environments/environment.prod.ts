d1312585006db0d6ff47776a2ad624ee9499f546
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-04-12 16:06:46 +0200

    Endpoint in https mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://www.read-me.io',
-  version: require('../../package.json').version
+  api_url: "https://www.read-me.io",
+  version: require("../../package.json").version
 };
